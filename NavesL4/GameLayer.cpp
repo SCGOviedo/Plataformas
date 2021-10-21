e8099c15646cde61ea6b5980f08763238f07f4df
Sergio Castillo García <UO276436@uniovi.es>
2021-10-21 14:41:07 +0200

    quitado el coutAgregar archivos de proyecto.

---
+++ NavesL4/GameLayer.cpp
@@ -0,0 +1,332 @@
+#include "GameLayer.h"
+
+GameLayer::GameLayer(Game* game)
+	: Layer(game) {
+	//llama al constructor del padre : Layer(renderer)
+	init();
+}
+
+
+void GameLayer::init() {
+	space = new Space(1);
+	scrollX = 0;
+	tiles.clear();
+
+	audioBackground = new Audio("res/musica_ambiente.mp3", true);
+	audioBackground->play();
+
+	points = 0;
+	textPoints = new Text("hola", WIDTH * 0.92, HEIGHT * 0.04, game);
+	textPoints->content = to_string(points);
+
+	
+	background = new Background("res/fondo_2.png", WIDTH * 0.5, HEIGHT * 0.5, -1, game);
+	backgroundPoints = new Actor("res/icono_puntos.png",
+		WIDTH * 0.85, HEIGHT * 0.05, 24, 24, game);
+
+	enemies.clear(); // Vaciar por si reiniciamos el juego
+	projectiles.clear(); // Vaciar por si reiniciamos el juego
+
+	loadMap("res/0.txt");
+}
+
+void GameLayer::loadMap(string name) {
+	char character;
+	string line;
+	ifstream streamFile(name.c_str());
+	if (!streamFile.is_open()) {
+		cout << "Falla abrir el fichero de mapa" << endl;
+		return;
+	}
+	else {
+		// Por línea
+		for (int i = 0; getline(streamFile, line); i++) {
+			istringstream streamLine(line);
+			mapWidth = line.length() * 40; // Ancho del mapa en pixels
+			// Por carácter (en cada línea)
+			for (int j = 0; !streamLine.eof(); j++) {
+				streamLine >> character; // Leer character 
+				cout << character;
+				float x = 40 / 2 + j * 40; // x central
+				float y = 32 + i * 32; // y suelo
+				loadMapObject(character, x, y);
+			}
+
+			cout << character << endl;
+		}
+	}
+	streamFile.close();
+}
+
+void GameLayer::loadMapObject(char character, float x, float y)
+{
+	switch (character) {
+	case 'E': {
+		Enemy* enemy = new Enemy(x, y, game);
+		// modificación para empezar a contar desde el suelo.
+		enemy->y = enemy->y - enemy->height / 2;
+		enemies.push_back(enemy);
+		space->addDynamicActor(enemy);
+		break;
+	}
+	case '1': {
+		player = new Player(x, y, game);
+		// modificación para empezar a contar desde el suelo.
+		player->y = player->y - player->height / 2;
+		space->addDynamicActor(player);
+		break;
+	}
+	case '#': {
+		Tile* tile = new Tile("res/bloque_tierra.png", x, y, game);
+		// modificación para empezar a contar desde el suelo.
+		tile->y = tile->y - tile->height / 2;
+		tiles.push_back(tile);
+		space->addStaticActor(tile);
+		break;
+	}
+	}
+}
+
+
+void GameLayer::processControls() {
+	// obtener controles
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		keysToControls(event);
+	}
+	//procesar controles
+	//procesar controles
+	// Disparar
+	if (controlShoot) {
+		Projectile* newProjectile = player->shoot();
+		if (newProjectile != NULL) {
+			space->addDynamicActor(newProjectile);
+			projectiles.push_back(newProjectile);
+		}
+
+	}
+
+	// Eje X
+	if (controlMoveX > 0) {
+		player->moveX(1);
+	}
+	else if (controlMoveX < 0) {
+		player->moveX(-1);
+	}
+	else {
+		player->moveX(0);
+	}
+
+	// Eje Y
+	if (controlMoveY > 0) {
+	
+	}
+	else if (controlMoveY < 0) {
+		player->jump();
+	}
+	else {
+
+	}
+
+
+
+}
+
+void GameLayer::update() {
+	// Jugador se cae
+	if (player->y > HEIGHT + 80) {
+		init();
+	}
+
+	space->update();
+	background->update();
+	player->update();
+	for (auto const& enemy : enemies) {
+		enemy->update();
+	}
+	for (auto const& projectile : projectiles) {
+		projectile->update();
+	}
+
+
+	// Colisiones
+	for (auto const& enemy : enemies) {
+		if (player->isOverlap(enemy)) {
+			player->loseLife();
+			if (player->lifes <= 0) {
+				init();
+				return;
+			}
+		}
+	}
+
+	// Colisiones , Enemy - Projectile
+
+	list<Enemy*> deleteEnemies;
+	list<Projectile*> deleteProjectiles;
+	for (auto const& projectile : projectiles) {
+		if (projectile->isInRender(scrollX) == false || projectile->vx == 0) {
+
+			bool pInList = std::find(deleteProjectiles.begin(),
+				deleteProjectiles.end(),
+				projectile) != deleteProjectiles.end();
+
+			if (!pInList) {
+				deleteProjectiles.push_back(projectile);
+			}
+		}
+	}
+
+
+
+	for (auto const& enemy : enemies) {
+		for (auto const& projectile : projectiles) {
+			if (enemy->isOverlap(projectile)) {
+				bool pInList = std::find(deleteProjectiles.begin(),
+					deleteProjectiles.end(),
+					projectile) != deleteProjectiles.end();
+
+				if (!pInList) {
+					deleteProjectiles.push_back(projectile);
+				}
+
+
+				enemy->impacted();
+				points++;
+				textPoints->content = to_string(points);
+
+
+			}
+		}
+	}
+
+	for (auto const& enemy : enemies) {
+		if (enemy->state == game->stateDead) {
+			bool eInList = std::find(deleteEnemies.begin(),
+				deleteEnemies.end(),
+				enemy) != deleteEnemies.end();
+
+			if (!eInList) {
+				deleteEnemies.push_back(enemy);
+			}
+		}
+	}
+
+	for (auto const& delEnemy : deleteEnemies) {
+		enemies.remove(delEnemy);
+		space->removeDynamicActor(delEnemy);
+	}
+	deleteEnemies.clear();
+
+	for (auto const& delProjectile : deleteProjectiles) {
+		projectiles.remove(delProjectile);
+		space->removeDynamicActor(delProjectile);
+		delete delProjectile;
+	}
+	deleteProjectiles.clear();
+
+
+}
+
+void GameLayer::calculateScroll() {
+	// limite izquierda
+	if (player->x > WIDTH * 0.3) {
+		if (player->x - scrollX < WIDTH * 0.3) {
+			scrollX = player->x - WIDTH * 0.3;
+		}
+	}
+
+	// limite derecha
+	if (player->x < mapWidth - WIDTH * 0.3) {
+		if (player->x - scrollX > WIDTH * 0.7) {
+			scrollX = player->x - WIDTH * 0.7;
+		}
+	}
+}
+
+
+void GameLayer::draw() {
+	calculateScroll();
+
+	background->draw();
+	for (auto const& tile : tiles) {
+		tile->draw(scrollX);
+	}
+
+	for (auto const& projectile : projectiles) {
+		projectile->draw(scrollX);
+	}
+
+	player->draw(scrollX);
+	for (auto const& enemy : enemies) {
+		enemy->draw(scrollX);
+	}
+
+	backgroundPoints->draw();
+	textPoints->draw();
+	SDL_RenderPresent(game->renderer); // Renderiza
+}
+
+void GameLayer::keysToControls(SDL_Event event) {
+	if (event.type == SDL_KEYDOWN) {
+		int code = event.key.keysym.sym;
+		// Pulsada
+		switch (code) {
+		case SDLK_ESCAPE:
+			game->loopActive = false;
+			break;
+		case SDLK_1:
+			game->scale();
+			break;
+		case SDLK_d: // derecha
+			controlMoveX = 1;
+			break;
+		case SDLK_a: // izquierda
+			controlMoveX = -1;
+			break;
+		case SDLK_w: // arriba
+			controlMoveY = -1;
+			break;
+		case SDLK_s: // abajo
+			controlMoveY = 1;
+			break;
+		case SDLK_SPACE: // dispara
+			controlShoot = true;
+			break;
+		}
+
+
+	}
+	if (event.type == SDL_KEYUP) {
+		int code = event.key.keysym.sym;
+		// Levantada
+		switch (code) {
+		case SDLK_d: // derecha
+			if (controlMoveX == 1) {
+				controlMoveX = 0;
+			}
+			break;
+		case SDLK_a: // izquierda
+			if (controlMoveX == -1) {
+				controlMoveX = 0;
+			}
+			break;
+		case SDLK_w: // arriba
+			if (controlMoveY == -1) {
+				controlMoveY = 0;
+			}
+			break;
+		case SDLK_s: // abajo
+			if (controlMoveY == 1) {
+				controlMoveY = 0;
+			}
+			break;
+		case SDLK_SPACE: // dispara
+			controlShoot = false;
+			break;
+		}
+
+	}
+
+}
+
